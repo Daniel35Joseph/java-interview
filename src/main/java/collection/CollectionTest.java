1e46eee4d9fd619542c92000b1090bac13b6b1f7
Daniel35Joseph <87067443+Daniel35Joseph@users.noreply.github.com>
2022-12-03 08:44:38 +0100

    Collection Test

--- src/main/java/collection/CollectionTest.java
+++ src/main/java/collection/CollectionTest.java
@@ -1,6 +1,9 @@
 package collection;
 
+import java.lang.Math;
+import java.io.*;
 import java.util.List;
+import java.util.ArrayList;
 
 /**
  * You should complete the function in this class
@@ -14,14 +17,35 @@ class CollectionTest {
    * operation : x -> ((x * 2) + 3) ^ 5
    */
   public static List<Double> compute1(List<Integer> input) {
-    return null;
+    List<Double> output = new ArrayList<>();
+    for(int i=0 ; i<input.size() ; i++){
+      double x = input.get(i);
+      output.add(Math.pow(((x * 2) + 3),5));
+    }
+    return output;
   }
 
   /**
    * operation : abc -> AbcAbc
    */
   public static List<String> compute2(List<String> input) {
-    return null;
+    List<String> output = new ArrayList<>();
+    
+    
+    for(int i=0 ; i<input.size() ; i++){
+      String text = input.get(i);
+      if (text.length() == 1){
+        text=text.toUpperCase();
+      }else if(text.length() == 0){
+        
+      }else{
+        String firstLetter = text.substring(0, 1).toUpperCase();
+        text = firstLetter+text.substring(1);
+      }
+      //text = text+text;
+      output.add(text+text);
+    } 
+    return output;
   }
 
 }
